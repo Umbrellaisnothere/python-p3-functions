a477390672d373f5d17353615891bd8cbc22155e
Keith Murimi <keith.murimi@student.moringaschool.com>
2024-08-27 11:41:30 +0300

    Completed the lab

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,21 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
+    print("Hello, programmer!")
     pass
 
-def greet(name):
+def greet(Keith):
+    print(f"Hello, {Keith}!")
     pass
 
 def greet_with_default(name="programmer"):
+    print(f"Hello, {name}!")
     pass
 
 def add(num1, num2):
+    return num1 + num2
     pass
 
-def halve(number):
+def halve(number = 12):
+    return number/2
     pass
